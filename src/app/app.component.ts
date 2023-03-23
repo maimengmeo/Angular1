bcc5f65b5a5681cfabe2bec641e3fb09c6f5e604
Mai Pham <mai291294@gmail.com>
2023-03-23 13:28:55 -0400

    import class to app compnent, create new student

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Student} from './myClasses/student';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'phamtuyeA3';
+
+  bio: Student = {
+    name: 'Tuyet Mai Pham',
+    homeCountry: 'Vietnam',
+    currentCountry: 'Canada',
+    id: 991545166,
+    login: 'phamtuye',
+    email: 'phamtuye@sheridancollge.ca',
+    image: 'assets/images/IMG_2305.jpg',
+    homeFlag: 'assets/images/vietnam.webp'
+  }
 }
