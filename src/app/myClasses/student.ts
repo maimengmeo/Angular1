cb7437ac6ae1341d47d9b8be71db6a67a3552195
Mai Pham <mai291294@gmail.com>
2023-03-23 13:20:41 -0400

    added student class

---
+++ src/app/myClasses/student.ts
@@ -0,0 +1,10 @@
+export class Student {
+    name!: string;
+    homeCountry!: string;
+    currentCountry!: string;
+    id!: number;
+    login!: string;
+    email!: string;
+    image!: string;
+    homeFlag!: string;
+}
